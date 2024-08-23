91ec3c8cf31c86256128cc26700fac3dc48a4a38
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-08-23 11:02:07 -0500

    Update main.tf
    updated subnets prefixes for new azureRM version

--- main.tf
+++ main.tf
@@ -223,12 +223,12 @@ resource "azurerm_virtual_network" "spoke-vnet" {
   name                = "AZ-spoke-vnet"
   resource_group_name = azurerm_resource_group.RG.name  
   subnet {
-    address_prefix     = "10.250.0.0/24"
+    address_prefixes     = ["10.250.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.spokevnetNSG.id
   }
   subnet {
-    address_prefix     = "10.250.1.0/24"
+    address_prefixes     = ["10.250.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   timeouts {
@@ -358,7 +358,7 @@ resource "azurerm_route_table" "RT" {
   name                          = "RT"
   location                      = azurerm_resource_group.RG.location
   resource_group_name           = azurerm_resource_group.RG.name
-  disable_bgp_route_propagation = false
+  
 
   route {
     name           = "toasa"
